30b2ae3a18862c8a548e573924016cde1820ff76
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2024-03-05 10:44:14 +0000

    main

--- TestApp.java
+++ TestApp.java
@@ -1,46 +1,31 @@
 package bubblesort;
 
-import java.util.ArrayList;
 /**
  *
  * @author Eskandar Atrakchi
- * @param <ElemType>
  */
-public class MyArrayList < ElemType > extends ArrayList < ElemType > {
-    
-    public void betterBubble () {
-        
-        Comparable elemAtJ;
-        Comparable elemAtJPlus;
-        
-        for (int i = 0; i < size (); i ++) {
-        
-            for (int j = 0; j < size () - 1 - i; j ++) {
-                
-                elemAtJ = (Comparable)get(j);
-                elemAtJPlus = (Comparable)get(j + 1);
-                
-                if (elemAtJ.compareTo(elemAtJPlus) > 0) {
-                    
-                    swap (j , j + 1); //call the method
-                    
-                }
-            }
-        }
-    }
-    
-    //swap method
-    private void swap (int position1 , int position2) {
-        
-        //temp holder for the elemenets 
-        // remove and add ur element to the correct position
-        
-        ElemType obj1 = get(position1);
-        ElemType obj2 = get(position2);
-        
-        set (position1 , obj2);
-        set (position2 , obj1);
+public class TestApp {
 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MyArrayList <Integer> list1 = new MyArrayList<Integer> ();
+        
+        list1.add(2);
+        list1.add(1);
+        list1.add(5);
+        list1.add(3);
+        list1.add(0);
+        
+        
+        System.out.println("before " + list1);
+
+        list1.betterBubble();
+        
+        System.out.println("After " + list1);
         
     }
+    
 }
